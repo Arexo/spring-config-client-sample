7d818911cd68dbad33defcfbc393a435644e319e
olivier.antoine <olivier.antoine@lampiris.be>
2020-12-01 19:31:29 +0100

    Initial commit

---
+++ src/test/java/be/arexo/conflig/client/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package be.arexo.conflig.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
